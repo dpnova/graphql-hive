357295d7fe97994eff8f24784fb515fe0ac4a3f5
Eddy Nguyen <github@eddeee888.me>
2024-06-21 21:16:07 +1000

    Migrate Mutation.deleteCdnAccessToken

--- packages/services/api/src/modules/cdn/resolvers.ts
+++ packages/services/api/src/modules/cdn/resolvers.ts
@@ -42,35 +42,5 @@ export const resolvers: CdnModule.Resolvers = {
         },
       };
     },
-    deleteCdnAccessToken: async (_, { input }, { injector }) => {
-      const translator = injector.get(IdTranslator);
-
-      const [organizationId, projectId, targetId] = await Promise.all([
-        translator.translateOrganizationId(input.selector),
-        translator.translateProjectId(input.selector),
-        translator.translateTargetId(input.selector),
-      ]);
-
-      const deleteResult = await injector.get(CdnProvider).deleteCDNAccessToken({
-        organizationId,
-        projectId,
-        targetId,
-        cdnAccessTokenId: input.cdnAccessTokenId,
-      });
-
-      if (deleteResult.type === 'failure') {
-        return {
-          error: {
-            message: deleteResult.reason,
-          },
-        };
-      }
-
-      return {
-        ok: {
-          deletedCdnAccessTokenId: input.cdnAccessTokenId,
-        },
-      };
-    },
   },
 };
