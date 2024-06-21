1a04af7884074931421cadabce8e2aaaa87f5b5e
Eddy Nguyen <github@eddeee888.me>
2024-06-21 21:11:36 +1000

    Migrate Target

--- packages/services/api/src/modules/cdn/resolvers.ts
+++ packages/services/api/src/modules/cdn/resolvers.ts
@@ -80,23 +80,4 @@ export const resolvers: CdnModule.Resolvers = {
       return cdn.isEnabled();
     },
   },
-  Target: {
-    async cdnAccessTokens(target, args, context) {
-      const result = await context.injector.get(CdnProvider).getPaginatedCDNAccessTokensForTarget({
-        targetId: target.id,
-        projectId: target.projectId,
-        organizationId: target.orgId,
-        first: args.first ?? null,
-        cursor: args.after ?? null,
-      });
-
-      return {
-        edges: result.items,
-        pageInfo: result.pageInfo,
-      };
-    },
-    cdnUrl(target, _args, context) {
-      return context.injector.get(CdnProvider).getCdnUrlForTarget(target.id);
-    },
-  },
 };
